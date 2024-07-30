93e703d37328109a3bc9e4f3d132fdfb546bf595
Danylo Yevsikov <6karai9@gmail.com>
2024-07-30 23:28:35 +0300

    fix id in SearchBox

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -4,7 +4,7 @@ import styles from './SearchBox.module.css';
 const SearchBox = ({ value, onChange }) => (
   <div className={styles.searchBox}>
     <h2>Find contacts by name</h2>
-    <input type="text" value={value} onChange={onChange} />
+    <input id="name" type="text" autoComplete="name" value={value} onChange={onChange} />
   </div>
 );
 
